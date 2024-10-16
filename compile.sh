3645be008066ed785abb00316361c200764a3a77
Janya Bysani <janyanb@uw.edu>
2024-10-16 00:10:37 -0700

    Files for Program1

--- compile.sh
+++ compile.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-g++ pi_monte.cpp Timer.cpp -o pi_monte
-g++ pi_monte_omp.cpp Timer.cpp -fopenmp -o pi_monte_omp
 
-g++ pi_integral.cpp Timer.cpp -o pi_integral
-g++ pi_integral_omp.cpp Timer.cpp -fopenmp -o pi_integral_omp
+g++ initialize.cpp -o initialize
+g++ -c EvalXOverMutate.cpp -fopenmp
+g++ -c Timer.cpp
+g++ Tsp.cpp Timer.o EvalXOverMutate.o -fopenmp -o Tsp
+
 
 
